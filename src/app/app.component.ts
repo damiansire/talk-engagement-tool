6843fa3b18e8205ef2c0130a3760fa66d8163351
Damian Sire <damiansire@gmail.com>
2024-11-09 02:06:50 -0300

    feat: show the players list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,24 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GithubService } from './services/github.service';
+import { PrizesComponent } from './components/prizes/prizes.component';
+import { PlayersInformationComponent } from './components/players-information/players-information.component';
+import { PlayersListComponent } from './components/players-list/players-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    PrizesComponent,
+    PlayersInformationComponent,
+    PlayersListComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'github-raffle';
 
-  private githubService = inject(GithubService);
-
-  ngOnInit() {
-    this.githubService.getStars().then((stars) => {
-      console.log('Stars:', stars);
-      // Do something with the stars data
-    });
-  }
+  ngOnInit() {}
 }
--- src/app/components/players-list/players-list.component.ts
+++ src/app/components/players-list/players-list.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { GithubService } from '../../services/github.service';
+import { PlayerCardComponent } from './player-card/player-card.component';
 
 @Component({
   selector: 'app-players-list',
   standalone: true,
-  imports: [],
+  imports: [PlayerCardComponent],
   templateUrl: './players-list.component.html',
-  styleUrl: './players-list.component.css'
+  styleUrl: './players-list.component.css',
 })
 export class PlayersListComponent {
-
+  githubService = inject(GithubService);
 }
