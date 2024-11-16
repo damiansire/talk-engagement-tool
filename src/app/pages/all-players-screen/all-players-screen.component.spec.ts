bc881f08e0b8c9ac0a226fce0f633404fcff09f9
Damian Sire <damiansire@gmail.com>
2024-11-16 11:08:11 -0300

    feat: add all players screen

---
+++ src/app/pages/all-players-screen/all-players-screen.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AllPlayersComponent } from './all-players-screen.component';
+
+describe('AllPlayersComponent', () => {
+  let component: AllPlayersComponent;
+  let fixture: ComponentFixture<AllPlayersComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AllPlayersComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AllPlayersComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/all-players-screen/all-players-screen.component.ts
@@ -0,0 +1,28 @@
+import { Component, inject } from '@angular/core';
+import { GithubService } from '../../services/github.service';
+import { PlayersInformationComponent } from '../../components/players-information/players-information.component';
+import { PlayersListComponent } from '../../components/players-list/players-list.component';
+import { PrizeListComponent } from '../../components/prize-list/prize-list.component';
+import { SortPriceListComponent } from '../../components/sort-price-list/sort-price-list.component';
+
+@Component({
+  selector: 'app-all-players-screen',
+  standalone: true,
+  imports: [
+    PlayersInformationComponent,
+    PlayersListComponent,
+    PrizeListComponent,
+    SortPriceListComponent,
+  ],
+  templateUrl: './all-players-screen.component.html',
+  styleUrl: './all-players-screen.component.css',
+})
+export class AllPlayersScreenComponent {
+  title = 'github-raffle';
+  githubService = inject(GithubService);
+  ngOnInit() {}
+
+  refresh() {
+    this.githubService.refresh();
+  }
+}
