c7736f09cc627fdf6231141a4d4728a792077e67
Damian Sire <damiansire@gmail.com>
2024-11-16 11:08:22 -0300

    feat: divide into routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { GithubService } from './services/github.service';
-import { PlayersInformationComponent } from './components/players-information/players-information.component';
-import { PlayersListComponent } from './components/players-list/players-list.component';
-import { PrizeListComponent } from './components/prize-list/prize-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    PlayersInformationComponent,
-    PlayersListComponent,
-    PrizeListComponent,
-  ],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'github-raffle';
-
-  ngOnInit() {}
-}
+export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AllPlayersScreenComponent } from './pages/all-players-screen/all-players-screen.component';
+import { RaffleScreenComponent } from './pages/raffle-screen/raffle-screen.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: AllPlayersScreenComponent },
+  { path: 'raffle', component: RaffleScreenComponent },
+];
