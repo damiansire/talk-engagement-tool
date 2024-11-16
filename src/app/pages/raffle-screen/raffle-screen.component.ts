0606b083180a5447edd709c7a6db645bd89820b0
Damian Sire <damiansire@gmail.com>
2024-11-16 19:35:04 -0300

    feat: add raffle screen

--- src/app/pages/raffle-screen/raffle-screen.component.ts
+++ src/app/pages/raffle-screen/raffle-screen.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Prize } from '../../interface/prizes.interface';
+import { GithubService } from '../../services/github.service';
+import { PrizesService } from '../../services/prizes.service';
+import { SortPrizeService } from '../../services/sort-prize.service';
+
+interface Participant {
+  id: number;
+  name: string;
+  photo: string;
+}
 
 @Component({
   selector: 'app-raffle-screen',
   standalone: true,
   imports: [],
   templateUrl: './raffle-screen.component.html',
-  styleUrl: './raffle-screen.component.css'
+  styleUrl: './raffle-screen.component.css',
 })
 export class RaffleScreenComponent {
+  githubService = inject(GithubService);
+  prizeService = inject(PrizesService);
+  sortService = inject(SortPrizeService);
+  constructor() {}
 
+  sortPrize() {
+    this.sortService.sortPrize();
+  }
 }
